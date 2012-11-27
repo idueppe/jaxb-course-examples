26084ad40cdb258a3cb28d8953e6fa5b810f6106
idueppe <ingo.dueppe@dueppe.com>
2012-11-27 13:20:43 +0100

    Enhanced XPathTest class

--- examples/xpath-examples/src/test/java/de/crowdcode/jaxb/xpath/XPathTest.java
+++ examples/xpath-examples/src/test/java/de/crowdcode/jaxb/xpath/XPathTest.java
@@ -1,10 +1,11 @@
 package de.crowdcode.jaxb.xpath;
 
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
+import org.w3c.dom.Text;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -12,7 +13,6 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
 import java.io.InputStream;
-import java.io.StringReader;
 
 
 /**
@@ -35,13 +35,25 @@ public class XPathTest {
         DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         Document document = builder.parse(is);
 
-        Object result = xpath.evaluate("//titel", document, XPathConstants.NODESET);
+        Object result = xpath.evaluate("//cdliste", document, XPathConstants.NODESET);
 
         NodeList nodeList = (NodeList) result;
+        printNodeList(nodeList, 0);
+    }
 
+    private void printNodeList(NodeList nodeList, int indent) {
         for (int i = 0; i < nodeList.getLength(); i++) {
-           Node node = nodeList.item(i);
-            System.out.printf(node.toString());
+            Node node = nodeList.item(i);
+            if (node instanceof Text) {
+                System.out.println(indent(indent)+" "+node.getTextContent());
+            } else {
+                System.out.println(indent(indent) + " <" + node.getNodeName() + ">");
+            }
+            printNodeList(node.getChildNodes(), indent + 1);
         }
+    }
+
+    private String indent(int indent) {
+        return StringUtils.repeat("\t", indent);
     }
 }
